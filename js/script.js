85621fbfc01282adaf51e90d7050f3ad00715d63
ssamartin <59620514+ssamartin@users.noreply.github.com>
2020-01-31 17:59:31 -0800

    updates

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // your script file for adding your own jquery
 $(function() {
 // Your Code from here on down. Don't delete that line above!
-  
 
 
 
@@ -17,4 +16,8 @@ $(function() {
 
 
 // End of Your Code . Don't delete that line below!!
+});
+
+$('#hp_button_one_id').click(function(){
+  $('html, body').animate({scrollTop: $("#second_page_id").offset().top});
 });
