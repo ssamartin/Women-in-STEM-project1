7828b26af7c2d421e14c6fc464844ab5705d5b33
ssamartin <59620514+ssamartin@users.noreply.github.com>
2020-02-04 22:24:42 -0800

    updates

--- js/script.js
+++ js/script.js
@@ -17,7 +17,15 @@ $(function() {
 
 // End of Your Code . Don't delete that line below!!
 });
+//$('#id of action button').click(function(){
+//  $('html, body').animate({scrollTop: $("#section id").offset().top});
+//});
+
 
 $('#hp_button_one_id').click(function(){
   $('html, body').animate({scrollTop: $("#second_page_id").offset().top});
+});
+
+$('#hp_button_two_id').click(function(){
+  $('html, body').animate({scrollTop: $("#third_page_id").offset().top});
 });
